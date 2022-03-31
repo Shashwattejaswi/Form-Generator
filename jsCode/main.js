d7ccd506c7d3b68952dead8a3d59b9c98ff7c292
Shashwattejaswi <102163060+Shashwattejaswi@users.noreply.github.com>
2022-03-31 19:57:37 +0530

    add function for glowing  checkbox
    function work is to checked and unchecked the checkbox with glowing style

--- jsCode/main.js
+++ jsCode/main.js
@@ -127,3 +127,17 @@ function checkNullValue(t){
 		return false;
 	}
 }
+
+function checkUncheckWithGlow(ele){
+	 if(ele .childNodes[1].checked==false)
+		 {
+			ele .childNodes[1].checked=true;
+			ele .style .cssText="border:3px solid skyblue;background:rgba(105,105,105);color:skyblue;text-shadow:0px 0px 1.8px rgba(135,206,235);box-shadow:0px 0px 7px rgba(135,206,235) inset;";
+		 }
+		 else
+		 {
+			ele .childNodes[1].checked=false;
+			ele .style.cssText="border:3px solid gray;color:rgb(105,105,105);";
+			ele .firstChild.style.cssText="text-shadow:0px 0px 0px skyblue";
+		 }	 
+	}
